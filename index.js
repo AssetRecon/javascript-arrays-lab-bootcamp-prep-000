188ae2e6eb8cd747086687bd0aeaa61990b78142
Edsel Caprice <caprice@mail.usf.edu>
2018-05-17 11:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ function destructivelyPrependKitten(name) {
   return kittens;
 }
 
-function destructivelyRemoveLastKitten(name) {
+function destructivelyRemoveLastKitten() {
   kittens.pop();
   return kittens;
 }
 
-function destructivelyRemoveFirstKitten(name) {
+function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;
 }
